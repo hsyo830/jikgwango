13f569e6dd5e2ee6f5e08480c0db1fd693747c3a
Seoyoung Hwang <nct830@naver.com>
2026-05-20 17:04:01 +0900

    feat : 구장 안내 페이지 지도 띄우기 -> 핀 이미지 변경

--- src/features/stadiums/map/StadiumMap.tsx
+++ src/features/stadiums/map/StadiumMap.tsx
@@ -18,53 +18,52 @@ type StadiumMapProps = {
 };
 
 const StadiumMap = ({ stadiumData }: StadiumMapProps) => {
-  // const mapRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   if (!window.kakao || !mapRef.current) {
-  //     console.log("카카오 SDK 또는 mapRef 없음");
-  //     return;
-  //   }
+  useEffect(() => {
+    if (!window.kakao || !mapRef.current) {
+      console.log("카카오 SDK 또는 mapRef 없음");
+      return;
+    }
 
-  //   const lat = Number(stadiumData.map?.lat);
-  //   const lng = Number(stadiumData.map?.lng);
+    const lat = Number(stadiumData.map?.lat);
+    const lng = Number(stadiumData.map?.lng);
 
-  //   console.log("위도 경도:", lat, lng);
+    console.log("위도 경도:", lat, lng);
 
-  //   if (Number.isNaN(lat) || Number.isNaN(lng)) return;
+    if (Number.isNaN(lat) || Number.isNaN(lng)) return;
 
-  //   window.kakao.maps.load(() => {
-  //     console.log("지도 생성 시작");
+    window.kakao.maps.load(() => {
+      console.log("지도 생성 시작");
 
-  //     const position = new window.kakao.maps.LatLng(lat, lng);
+      const position = new window.kakao.maps.LatLng(lat, lng);
 
-  //     const map = new window.kakao.maps.Map(mapRef.current, {
-  //       center: position,
-  //       level: 3,
-  //     });
+      const map = new window.kakao.maps.Map(mapRef.current, {
+        center: position,
+        level: 3,
+      });
 
-  //     const imageSrc = "/image/map-marker/map-marker.png";
-  //     const imageSize = new window.kakao.maps.Size(52, 64);
-  //     const imageOption = {
-  //       offset: new window.kakao.maps.Point(26, 64),
-  //     };
+      const imageSrc = "/image/map-marker/map-marker.png";
+      const imageSize = new window.kakao.maps.Size(47, 50);
+      const imageOption = {
+        offset: new window.kakao.maps.Point(23, 64),
+      };
 
-  //     const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+      const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
-  //     new window.kakao.maps.Marker({
-  //       map,
-  //       position,
-  //       image: markerImage,
-  //     });
-  //   });
-  // }, [stadiumData]);
-
-  // ref={mapRef}
+      new window.kakao.maps.Marker({
+        map,
+        position,
+        image: markerImage,
+      });
+    });
+  }, [stadiumData]);
 
   return (
-    <div className="relative h-45 w-full overflow-hidden rounded-xl md:h-60">
-      <Image src={mapImage} alt="야구장 지도" fill />
-    </div>
+    <div
+      ref={mapRef}
+      className="bg-surface-2 relative h-45 w-full overflow-hidden rounded-xl md:h-60"
+    />
   );
 };
 
