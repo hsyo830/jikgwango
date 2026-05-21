033591879c59fde7dd5eb66bdc5922020d2cec7d
Seoyoung Hwang <nct830@naver.com>
2026-05-21 23:25:53 +0900

    feat : 직관GO OGTAG 이미지 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,27 @@ import { pretendard } from "../styles/fonts";
 export const metadata: Metadata = {
   title: "직관GO | 야구 직관 정보를 한번에!",
   description: "KBO Baseball Stadium Information Platform",
+  openGraph: {
+    title: "직관GO",
+    description: "야구 직관 정보는 직관GO에서!",
+    url: "https://jikgwango.vercel.app",
+    siteName: "직관GO",
+    images: [
+      {
+        url: "https://jikgwango.vercel.app/jikgwango-ogtag.png",
+        width: 1200,
+        height: 630,
+        alt: "직관GO OG 이미지",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "직관GO",
+    description: "야구 직관 정보는 직관GO에서!",
+    images: ["/jikgwango-ogtag.png"],
+  },
 };
 
 export default function RootLayout({
