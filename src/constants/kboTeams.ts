6dc8709ed5f255630366a51bb43e2b6f5a89c090
Seoyoung Hwang <nct830@naver.com>
2026-05-24 18:02:31 +0900

    feat: 팀별 stadiumId 추가

--- src/constants/kboTeams.ts
+++ src/constants/kboTeams.ts
@@ -12,51 +12,61 @@ import SsgLogo from "@/public/image/team-logo/ssg-logo.svg";
 export const KBO_TEAMS = {
   LT: {
     id: "lotte-giants",
+    stadiumId: "busan",
     name: "롯데 자이언츠",
     logo: LotteLogo,
   },
   HH: {
     id: "hanwha-eagles",
+    stadiumId: "daejeon",
     name: "한화 이글스",
     logo: HanwhaLogo,
   },
   HT: {
     id: "kia-tigers",
+    stadiumId: "gwangju",
     name: "KIA 타이거즈",
     logo: KiaLogo,
   },
   WO: {
     id: "kiwoom-heroes",
+    stadiumId: "gocheok",
     name: "키움 히어로즈",
     logo: KiwoomLogo,
   },
   KT: {
     id: "kt-wiz",
+    stadiumId: "suwon",
     name: "KT 위즈",
     logo: KtLogo,
   },
   LG: {
     id: "lg-twins",
+    stadiumId: "jamsil",
     name: "LG 트윈스",
     logo: LgLogo,
   },
   NC: {
     id: "nc-dinos",
+    stadiumId: "changwon",
     name: "NC 다이노스",
     logo: NcLogo,
   },
   OB: {
     id: "doosan-bears",
+    stadiumId: "jamsil",
     name: "두산 베어스",
     logo: DoosanLogo,
   },
   SS: {
     id: "samsung-lions",
+    stadiumId: "daegu",
     name: "삼성 라이온즈",
     logo: SamsungLogo,
   },
   SK: {
     id: "ssg-landers",
+    stadiumId: "incheon",
     name: "SSG 랜더스",
     logo: SsgLogo,
   },
