e60cadeec0307675f7ec682b1ffbf8ad2dc4aa14
Seoyoung Hwang <nct830@naver.com>
2026-06-07 23:37:53 +0900

    feat: 검색어 입력 상태 관리 추가

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 import SearchIcon from "../icons/SearchIcon";
 
 const SearchBar = () => {
+  const [keyword, setKeyword] = useState("");
+
+  const handleChangeKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
   return (
     <form className="border-input-border bg-input h-8 w-full rounded-[7px] border md:h-10 lg:h-11">
       <div className="flex h-full min-w-0 items-center gap-2.5 px-2.5">
@@ -9,6 +19,8 @@ const SearchBar = () => {
           type="search"
           placeholder="원하는 팀의 정보를 검색해보세요."
           className="text-foreground placeholder:text-placeholder min-w-0 flex-1 bg-transparent text-sm outline-none focus:outline-none md:text-base"
+          value={keyword}
+          onChange={handleChangeKeyword}
         />
       </div>
     </form>
