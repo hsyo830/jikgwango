a3356ff08f43fe37b428e184424b3bd21bff4794
Seoyoung Hwang <nct830@naver.com>
2026-06-08 14:08:15 +0900

    feat : 음식부스페이지 - 음식 카드 레이아웃 깨짐 문제 해결 (UI 수정)

--- src/components/common/food-card/FoodCard.tsx
+++ src/components/common/food-card/FoodCard.tsx
@@ -61,7 +61,7 @@ const FoodCard = ({ food, variant = "main", index }: FoodProps) => {
         </div>
         <div className="text-muted flex items-center gap-0.5 py-1 text-[11px] md:gap-1 md:py-2.5 md:text-[12px] lg:pb-0 lg:text-sm">
           <MenuIcon className="text-primary h-4 w-4 shrink-0 md:h-6 md:w-6" />
-          <p className="text-primary w-10 font-bold md:w-12 lg:w-14">대표메뉴</p>
+          <p className="text-primary w-11 font-bold md:w-12 lg:w-14">대표메뉴</p>
           <p className="truncate font-medium">{foodCategory?.label}</p>
         </div>
       </div>
