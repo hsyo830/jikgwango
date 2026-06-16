c9fc9b386e788702a77caa211979f6acf03e65b0
Seoyoung Hwang <nct830@naver.com>
2026-06-16 12:04:10 +0900

    fix : fetchTodayGamesFromKBO 오류 확인 용 콘솔로그 삭제

--- src/services/kbo/fetchTodayGamesFromKBO.ts
+++ src/services/kbo/fetchTodayGamesFromKBO.ts
@@ -21,9 +21,6 @@ const fetchTodayGamesFromKBO = async (params: GameParams) => {
     responseType: "text",
   });
 
-  console.log("KBO status:", response.status);
-  console.log("KBO raw response:", response.data);
-
   return JSON.parse(response.data);
 };
 
